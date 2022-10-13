f9fd727bc0e575fba6e99c2acdbffa0ce5c269b2
root <root@DESKTOP-M366OAF.localdomain>
2022-10-13 09:53:23 -0400

    1

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
+// IMPORT/EXPORT LIBRARY //
+
 export {default as Main} from './Main'
+export {default as Navbar} from './Navbar'
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// DO NOT CHANGE THIS FILE //
+
 import React from 'react'
 import ReactDOM from "react-dom/client"
 import {Main} from './components'
